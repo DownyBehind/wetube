98ee4c30c6dcc5e4165be1bded81cf55905ea2ca
downy <TeamBehindDowny@gmail.com>
2019-07-10 20:28:42 +0900

    keep going2

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,7 +8,6 @@ import {
 } from "../controller/userController";
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDetail);
